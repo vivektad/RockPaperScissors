952fd2943270eba5a99d944af90cd339fb45449e
Vivek Tadikamalla <vivekt@u.nus.edu>
2021-09-25 21:20:45 +0800

    Calculate score based on computer and player input

--- scripts.js
+++ scripts.js
@@ -5,3 +5,17 @@ function computerPlay() {
     return array[randomNumber];
 }
 
+function round(playerSelection, computerSelection) {
+    // not considering situation when the wrong information/spelling mistakes are inserted
+    playerSelection = playerSelection.toLowerCase();
+    if (playerSelection === computerSelection) {
+        return 'There is no winner this round!';
+    } else if (playerSelection === 'rock' && computerSelection === 'paper' ||
+        playerSelection === 'paper' && computerSelection === 'scissors' ||
+            playerSelection === 'scissors' && computerSelection === 'rock') {
+        return 'You Lose! Paper beats Rock';
+    } else {
+        return 'You are a winner!';
+    }
+}
+
